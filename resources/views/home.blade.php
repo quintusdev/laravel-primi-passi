0e098c990b67f8c7d4ee8a45c502ebf8e9476591
Quintusdev <quintusdev92@gmail.com>
2023-07-20 13:43:05 +0200

    ES. BASE COMPLETATO

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <!-- BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
     <body>
-        <h1>Hello World! Primo Esercizio Laravel!</h1>
+        <div class="container">
+            <div class="row">
+                <div class="text-center mt-5">
+                    <h1>Hello World! Primo Esercizio Laravel!</h1>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
